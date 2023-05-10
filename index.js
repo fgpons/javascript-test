db9ac6aaded04e28e6b6e151db0141667d75263f
fgpons <facundog.pons@gmail.com>
2023-05-10 00:21:42 -0300

    added methods

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 console.log("hello world")
+
+let user = 19;
+let limiteEdad = 18;
+
+if (user > limiteEdad) {
+    prompt("Bienvenido.")
+} else {
+    prompt("No puedes ingresar, debes ser mayor de edad.")
+}
---
+++ methods.js
@@ -0,0 +1,7 @@
+let num1 = 45;
+let num2 = 20;
+let num3 = 100;
+
+
+console.log(num1 > num2, num2 < num3, num3 > num2, num2 < num1, num1 < num3)
+
